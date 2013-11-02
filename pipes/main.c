9deaf8ebb3422dd76a36614661bcaf465b805d26
Jakub Mejtský <jmejtsky@gmail.com>
2013-11-02 10:25:39 +0100

    Initial Commit

---
+++ pipes/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  pipes
+//
+//  Created by Jakub Mejtský on 02.11.13.
+//  Copyright (c) 2013 Jakub Mejtský. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
